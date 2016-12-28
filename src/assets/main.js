c5017deef19fcc5ce6302abfd77f25aa6a673b33
anthonyBetancourt00 <anthony.betancourt@helloworld.com>
2016-12-28 16:26:37 -0500

    updating the main.js file

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,38 @@ $(function() {
     url: 'https://www.codeschool.com/users/anthonybetancount.json',
     dataType: 'jsonp',
     success: function(response) {
-      // handle response here
+      console.log("response", response.courses.completed);
+      addCourses(response.courses.completed);
     }
   });
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      var $course = $('<div />', {
+          'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />' , {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        text: 'See Course',
+        target: '_blank',
+        href: course.url
+      }).appendTo($course);
+
+    })
+
+  }
 
 });
